b552bf3e94b4ce830ce00a29f16626bea8362b1c
joaovcb <joaovcb@kyros.com.br>
2025-09-01 19:16:07 -0300

    new firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -21,7 +21,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCsxQIOpLMwfvgB0gGX2mrxF2zmU-IUB-U",
   authDomain: "kd-racha.firebaseapp.com",
   projectId: "kd-racha",
-  storageBucket: "kd-racha.appspot.com",
+  storageBucket: "kd-racha.firebasestorage.app",
   messagingSenderId: "418214270856",
   appId: "1:418214270856:web:01a057af9bbabb48f74c3d",
   measurementId: "G-DS2EEYBY0V"
